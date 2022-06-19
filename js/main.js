6a94a89d87002f004018336212849d3450c98b16
NoTreblee <fiercestword@gmail.com>
2022-06-20 01:00:40 +0200

    fixed nav icon

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const nav = document.getElementById('nav');
 const navBtn = document.getElementById('burgerBtn');
 const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
+
 const handleNav = () => {
   nav.classList.toggle('nav--active');
+  navBtnBars.classList.remove('black-bars-color')
   allNavItems.forEach((item) => {
     item.addEventListener('click', () => {
       nav.classList.remove('nav--active');
@@ -10,6 +15,7 @@ const handleNav = () => {
   });
   handleNavItemsAnimation();
 };
+
 const handleNavItemsAnimation = () => {
   let delayTime = 1;
   allNavItems.forEach((item) => {
@@ -18,4 +24,28 @@ const handleNavItemsAnimation = () => {
     delayTime++;
   });
 };
+
+const handleObserver = () => {
+  const currentSection = window.scrollY;
+  allSections.forEach((section) => {
+    if (
+      section.classList.contains('white-section') &&
+      section.offsetTop <= currentSection + 60
+    ) {
+      navBtnBars.classList.add('black-bars-color');
+    } else if (
+      !section.classList.contains('white-section') &&
+      section.offsetTop <= currentSection + 60
+    ) {
+      navBtnBars.classList.remove('black-bars-color');
+    }
+  });
+};
+
+const handleCurrentYear = () => {
+  const year = new Date().getFullYear();
+  footerYear.innerText = year;
+};
+handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver)
